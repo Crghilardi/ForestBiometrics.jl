d744fe28a1abfdc8d8b87a5191fc609ea0311bbb
Casey Ghilardi <Crghilardi@gmail.com>
2017-12-23 22:23:53 -0800

    add A and B lines and stk % labels to gingrich chart

--- src/GingrichStocking_chart.jl
+++ src/GingrichStocking_chart.jl
@@ -8,7 +8,7 @@ using DataFrames
 function gingrich_chart(tpa_in,basal_area_in)
 #define parameters
 a =[-0.0507, 0.1698, 0.0317]
-b =[0.0175, 0.205, 0.06]
+b =[0.175, 0.205, 0.06]
 
 ba_ticks=collect(0:20:160)
 tpa_ticks=collect(0:50:450)
@@ -67,32 +67,52 @@ end
 #equation for A-line
 A_line=make_stklines(100,dia)
 
+stk_percent
 function make_bline(qmd)
+  STK=Int64[]
   BA=Float64[]
   TPA=Float64[]
   QMD=Int[]
   for i in stk_percent
     tpa= 1*(i*10)/(b[1]+b[2]*amd_convert(qmd)+b[3]*qmd^2)
     ba=pi*(qmd/24)^2*tpa
+    push!(STK,i)
     push!(TPA,tpa)
     push!(BA,ba)
     push!(QMD,qmd)
   end
-return DataFrame(TPA=TPA,BA=BA,QMD=QMD)
+return DataFrame(STK=STK,TPA=TPA,BA=BA,QMD=QMD)
 end
 
-df3=DataFrame(TPA=Float64[],BA=Float64[],QMD=Int[])
+df3=DataFrame(STK=Int64[],TPA=Float64[],BA=Float64[],QMD=Int[])
 for i in dia
   append!(df3,make_bline(i))
 end
-@df df plot(:TPA,:BA,group=:QMD,color=:black,legend=false)
+df3
+B_line=df3[df3[:STK] .== 100,:] # super hacky work around for now...
 
-@df df2 plot!(:TPA,:BA,group=:STK,color=:black,
+
+@df df plot(:TPA,:BA,group=:QMD,color=:black,legend=false)
+@df df2 plot!(:TPA,:BA,group=:STK,color=:black,legend=false)
+@df A_line plot!(:TPA,:BA,group=:STK,color=:red,legend=false)
+@df B_line plot!(:TPA,:BA,group=:STK,color=:red,
 xlabel="Trees Per Acre",
 ylabel="Basal Area (sq ft./acre)",
 xticks=collect(0:50:450),
 yticks=collect(0:20:200),
 legend=false)
+#text coords approximated, text() is a plot thing that wraps into type, so you can passs font and text parameters
+annotate!([(100, 19, text("20%",10)),
+           (138, 29, text("30%",10)),
+           (174, 38, text("40%",10)),
+           (214, 49, text("50%",10)),
+           (254, 60, text("60%",10)),
+           (290, 69, text("70%",10)),
+           (324, 79, text("80%",10)),
+           (364, 89, text("90%",10)),
+           (404, 99, text("100%",10)),
+           (440, 112, text("110%",10))
+           ])
 scatter!((tpa_in,basal_area_in))
 
 #####################
